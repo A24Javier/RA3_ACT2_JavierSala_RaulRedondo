67b28c00ad8022746729cf30399e6980c6d492fd
A24Javier <a24javsalcue@inspedralbes.cat>
2026-04-30 20:41:10 +0200

    Creando layout principal

---
+++ app/src/main/java/com/example/ra3_act2_javiersala_raulredondo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ra3_act2_javiersala_raulredondo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
